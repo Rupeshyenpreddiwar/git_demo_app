76aace0310aa11b9cd5b92345b1d88869b0ccdf8
rupeshyenpreddiwar <rupeshyen2673@gmail.com>
2024-05-26 14:16:58 +0530

    stramlit home page

---
+++ test.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title('Rupesh Yenpreddiwar')
+
+col1, col2 =st.columns(2)
+with col1:
+    st.image('rupesh image.jpg')
+
+with col2:
+    st.write('Download for Windows')
